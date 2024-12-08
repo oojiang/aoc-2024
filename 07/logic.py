8410c539aa9aa06bdc046c7f28b30427637a9797
Oliver Jiang <ojiang@berkeley.edu>
2024-12-07 23:38:03 -0800

    Day 07 part 1 fast solution

--- 07/logic.py
+++ 07/logic.py
@@ -22,29 +22,24 @@ def solution1(equations: List[Equation]):
 
 def is_possible(equation: Equation):
     expected_result, operands = equation
+    if len(operands) == 1:
+        return expected_result == operands[0]
+
+    reduced_operands = operands[:-1]
     
-    queue = [[Operator.ADD for _ in range(len(operands) - 1)]]
-    while queue:
-        operators = queue.pop()
-        result = compute_result(operands, operators)
-        if result == expected_result:
+    add_result = expected_result - operands[-1]
+    add_equation = (add_result, reduced_operands)
+    if is_possible(add_equation):
+        return True
+    
+    if expected_result % operands[-1] == 0:
+        mul_result = expected_result // operands[-1]
+        mul_equation = (mul_result, reduced_operands)
+        if is_possible(mul_equation):
             return True
-        else:
-            queue = queue + get_next_operators(operators)
-    return False
-        
+            
 def compute_result(operands: List[int], operators: List[Operator]) -> int:
     result = operands[0]
     for i in range(len(operators)):
         result = operators[i].apply(result, operands[i+1])
     return result
-
-def get_next_operators(operators: List[Operator]) -> List[List[Operator]]:
-    next_operators = []
-    for i in range(len(operators)):
-        if operators[i] == Operator.ADD:
-            next_operators.append(
-                [Operator.MULTIPLY if j == i else operators[j] for j in range(len(operators))]
-            )
-    return next_operators
-            
