3dc1bb3a38afc8782cf5aa6e720e5af42f6b1ca4
Oliver Jiang <ojiang@berkeley.edu>
2024-12-08 21:45:09 -0800

    Day 08 part 2

--- 08/logic.py
+++ 08/logic.py
@@ -9,6 +9,12 @@ def solution1(antenna_map: AntennaMap) -> int:
     filtered_antinodes = filter_antinodes(antenna_map, unioned_antinodes)
     return len(filtered_antinodes)
 
+def solution2(antenna_map: AntennaMap) -> int:
+    antennas = get_antennas(antenna_map)
+    resonances = [get_resonances_all(antenna_group, antenna_map) for antenna_group in antennas]
+    unioned_resonances = set().union(*resonances)
+    return len(unioned_resonances)
+
 def get_antennas(antenna_map: AntennaMap) -> List[List[Coordinate]]:
     antenna_groups = {}
     for r in range(len(antenna_map)):
@@ -38,7 +44,6 @@ def get_antinode_pair(
     antinode1 = (r1 + r12, c1 + c12)
     antinode2 = (r2 + r21, c2 + c21)
     return antinode1, antinode2
-        
 
 def filter_antinodes(antenna_map: AntennaMap, antinodes: Set[Coordinate]) -> Set[Coordinate]:
     return set(filter(lambda antinode: in_bounds(antenna_map, antinode), antinodes))
@@ -46,3 +51,31 @@ def filter_antinodes(antenna_map: AntennaMap, antinodes: Set[Coordinate]) -> Set
 def in_bounds(antenna_map: AntennaMap, coor: Coordinate) -> bool:
     r, c = coor
     return len(antenna_map) > r >= 0 and len(antenna_map[0]) > c >= 0
+
+def get_resonances_all(
+        antennas: List[Coordinate],
+        antenna_map: AntennaMap
+    ) -> Set[Coordinate]:
+    resonances = set()
+    pairs = itertools.combinations(antennas, 2)
+    for antenna1, antenna2 in pairs:
+        resonances.update(get_resonances(antenna1, antenna2, antenna_map))
+    return resonances
+
+def get_resonances(
+        antenna1: Coordinate,
+        antenna2: Coordinate,
+        antenna_map: AntennaMap
+    ) -> Set[Coordinate]:
+    vec = (antenna1[0] - antenna2[0], antenna1[1] - antenna2[1])
+
+    resonances = set()
+    next_resonance = antenna1
+    while in_bounds(antenna_map, next_resonance):
+        resonances.add(next_resonance)
+        next_resonance = (next_resonance[0] + vec[0], next_resonance[1] + vec[1])
+    next_resonance = antenna2
+    while in_bounds(antenna_map, next_resonance):
+        resonances.add(next_resonance)
+        next_resonance = (next_resonance[0] - vec[0], next_resonance[1] - vec[1])
+    return resonances
---
+++ 08/solution2.py
@@ -0,0 +1,6 @@
+from state import read_input
+from logic import solution2
+
+assert(solution2(read_input("input1")) == 34)
+print(solution2(read_input("input")))
+
