ec2676c295bb79521f2230d6b922896750d5a573
Oliver Jiang <ojiang@berkeley.edu>
2024-12-15 12:13:34 -0800

    Day 12 part 1 (v2)

--- 12/logic.py
+++ 12/logic.py
@@ -17,27 +17,41 @@ def solution1(garden: List[List[str]]) -> int:
 
 # Returns a matrix with the area of the region that a plot belongs to.
 def get_areas(garden: List[List[str]]) -> List[List[int]]:
-    regions = get_regions(garden)
-
-    areas = [[len(region) for region in row] for row in regions]
-    return areas
-
-def get_regions(garden: List[List[str]]) -> List[List[Set[Plot]]]:
-    region = [[set([(r,c)]) for c in range(len(garden[0]))] for r in range(len(garden))]
+    areas = [[1 for _ in row] for row in garden]
+    visited = [[False for _ in row] for row in garden]
 
     for r in range(len(garden)):
         for c in range(len(garden[0])):
-            for direction in (Direction.UP, Direction.LEFT):
-                if not is_border(garden, r, c, direction):
-                    adj_r = r + direction.vec[0]
-                    adj_c = c + direction.vec[1]
-                    if region[adj_r][adj_c] is not region[r][c]:
-                        region[adj_r][adj_c].update(region[r][c])
-                        for region_r, region_c in region[r][c]:
-                            region[region_r][region_c] = region[adj_r][adj_c]
+            if not visited[r][c]:
+                region = get_region(garden, (r, c))
+                for plot in region:
+                    visited[plot[0]][plot[1]] = True
+                    areas[plot[0]][plot[1]] = len(region)
+    return areas
+                
+
+def get_region(garden: List[List[str]], plot: Plot) -> Set[Plot]:
+    region = set()
+    visited = [[False for _ in row] for row in garden]
+    queue = []
+    
+    region.add(plot)
+    visited[plot[0]][plot[1]] = True
+    queue.append(plot)
+
+    while queue:
+        r, c = queue.pop()
+        for direction in Direction:
+            adj_r = r + direction.vec[0]
+            adj_c = c + direction.vec[1]
+            if len(garden) > adj_r >= 0 and len(garden[0]) > adj_c >= 0 \
+                and not visited[adj_r][adj_c] \
+                and garden[adj_r][adj_c] == garden[r][c]:
+                visited[adj_r][adj_c] = True
+                region.add((adj_r, adj_c))
+                queue.append((adj_r, adj_c))
 
     return region
-    
 
 def get_price(garden: List[List[str]], area: List[List[int]]) -> int:
     price = 0
@@ -55,16 +69,3 @@ def is_border(garden, r, c, direction):
         return garden[adj_r][adj_c] != garden[r][c]
     else:
         return True
-
-def print_matrix(matrix):
-    for row in matrix:
-        for ele in row:
-            print(ele, end='\t')
-        print()
-
-def print_garden_areas(garden, area):
-    for r in range(len(garden)):
-        for c in range(len(garden[0])):
-            print(garden[r][c], area[r][c], end='\t')
-        print()
-
