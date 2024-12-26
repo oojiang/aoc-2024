35630c99fcf5a0661d6731f553610588d35b11ad
Oliver Jiang <ojiang@berkeley.edu>
2024-12-25 21:21:33 -0800

    Day 19 part 2

--- 19/logic.py
+++ 19/logic.py
@@ -1,9 +1,16 @@
-from typing import List
+from typing import List, Optional, Dict
 
 def solution1(available: List[str], desired: List[str]) -> int:
     count = 0
     for pattern in desired:
         count += is_possible(available, pattern)
+    return count
+
+def solution2(available: List[str], desired: List[str]) -> int:
+    count = 0
+    cache = {}
+    for pattern in desired:
+        count += num_possible(available, pattern, cache)
     return count
 
 def is_possible(available: List[str], desired_pattern: str) -> bool:
@@ -24,6 +31,24 @@ def is_possible(available: List[str], desired_pattern: str) -> bool:
     
     return False
 
+def num_possible(available: List[str], desired_pattern: str, cache: Optional[Dict] = None) -> int:
+    if cache is not None and desired_pattern in cache:
+        return cache[desired_pattern]
+
+    if desired_pattern == "":
+        if cache is not None:
+            cache[desired_pattern] = 1
+        return 1
+
+    count = 0
+
+    for prefix in available:
+        if has_prefix(desired_pattern, prefix):
+            count += num_possible(available, desired_pattern[len(prefix):], cache)
+
+    if cache is not None:
+        cache[desired_pattern] = count
+    return count
+    
 def has_prefix(pattern: str, prefix: str) -> bool:
     return pattern[:len(prefix)] == prefix
-
---
+++ 19/solution2.py
@@ -0,0 +1,6 @@
+from state import read_input
+from logic import solution2
+
+# print(solution2(*read_input("input1")))
+# assert(solution2(*read_input("input1")) == 16)
+print(solution2(*read_input()))
